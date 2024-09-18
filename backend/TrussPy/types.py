5756c0b6c3ac6bc1885f01cf00b936d4d5a939b2
Nouchitic <hotaru105@outlook.com>
2024-09-18 10:44:13 +0800

    定义桁架数据结构

--- backend/TrussPy/types.py
+++ backend/TrussPy/types.py
@@ -1,18 +1,46 @@
-from typing import TypedDict
+from typing import List, TypedDict
 
+
+# 点坐标数据结构
+class Point(TypedDict):
+    x: float
+    y: float
+    is_Constraint: bool
+
+# 杆件数据结构
+class Line(TypedDict):
+    start: int
+    end: int
+    E: float
+    A: float
+    L: float
+    k: float
+
+# 应力杆件数据结构
+class Line_Force(TypedDict):
+    start: int
+    end: int
+    sigma: float
+
+# 载荷数据结构
+class Load(TypedDict):
+    point: int
+    Fx: float
+    Fy: float
 
 # 前端通过GET方法传递的桁架结构相关的输入数据
-class FrontendInputData(TypedDict):
-    ...
-
-# 后端在计算前对前端数据进行预处理后的数据结构
-class PreprocessedData(TypedDict):
-    ...
+class Frontend_Input(TypedDict):
+    point: List[Point]
+    line: List[Line]
+    load: List[Load]
 
 # 后端进行桁架结构计算后生成的结果数据
-class CalculationResultData(TypedDict):
-    ...
+class Calculation_Result(TypedDict):
+    point: List[Point]
+    line: List[Line]
+    load: List[Load]
 
 # 后端将计算结果处理后返回给前端，用于可视化展示的结果数据
-class VisualizationData(TypedDict):
-    ...
+class Visualization(TypedDict):
+    point: List[Point]
+    line: List[Line_Force]
