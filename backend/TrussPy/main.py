9224c9f510975b502ad0c17048284b1d93277c3b
nouchi <hotaru105@outlook.com>
2024-09-19 20:46:13 +0800

    commit message: ``` 后台添加载荷矩阵构建及LULT分解求解器
    在'backend/TrussPy/main.py'中扩展了主函数以包含载荷矩阵的构建。此外，在'TrussPy/truss.py'中添加了LULT分解求解器函数，使用scipy库进行高效求解。同时，优化了元素刚度矩阵计算函数，并增加了函数注释。
    ```

--- backend/TrussPy/main.py
+++ backend/TrussPy/main.py
@@ -7,13 +7,24 @@ def main(imput_data: Frontend_Input_Data):
         idx_num = len(imput_data['point']) - imput_data['constraint_nums']
         global_stiffness_matrix = np.zeros((idx_num*2, idx_num*2))
         point_map = Bidirectional_Map()
+
+        # 循环杆件构建总体刚度矩阵
         for line in imput_data['line']:
             for point_idx in line['points']:
+
+                # 根据节点类型分别构建
                 if imput_data['point'][point_idx]['Constraint_Type'] == 0:
                     gsm_idx = point_map.get_idx(point_idx) * 2
                     esm = Get_Element_Stiffness_Matrix(line['k'], line['theta'])
                     global_stiffness_matrix[gsm_idx:gsm_idx+2, gsm_idx:gsm_idx+2] += esm
+
                 elif imput_data['point'][point_idx]['Constraint_Type'] == 1:
                     # TODO 滑动铰支座
                     ...
-                
+
+        load_matrix = np.zeros((idx_num*2,1))
+        # 循环荷载构建载荷矩阵
+        for load in imput_data['load']:
+            idx = point_map.get_idx(load['point'])
+            load_matrix[idx*2] += load['Fx']
+            load_matrix[idx*2+1] += load['Fy']
--- backend/TrussPy/truss.py
+++ backend/TrussPy/truss.py
@@ -1,8 +1,23 @@
 import numpy as np
+import scipy
+import scipy.linalg
 
 
-def Get_Element_Stiffness_Matrix(k:float, theta: float):
+def Get_Element_Stiffness_Matrix(k: float, theta: float) -> np.ndarray:
+    '''计算平面桁架杆件单元刚度矩阵
+
+    输入参数：
+    k: 刚度系数
+    theta: 杆件方向与X轴夹角
+
+    返回值：
+    K_matrix: 杆件单元刚度矩阵
+    '''
     return np.array([
         [k * np.cos(theta) ** 2, k * np.cos(theta) * np.sin(theta)],
         [k * np.cos(theta) * np.sin(theta), k * np.sin(theta) ** 2]
     ])
+
+def LULT_Decomposition_Solver(gsm: np.ndarray, b: np.ndarray) -> np.ndarray:
+    # TODO 待实现
+    return scipy.linalg.solve(gsm, b)
