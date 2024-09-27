27c1cecc2d47049903ccc8ae34f134d2b13561bf
Nouchitic <hotaru105@outlook.com>
2024-09-27 17:34:25 +0800

    li

--- react-frontend/src/page/Truss/draw/Left.tsx
+++ react-frontend/src/page/Truss/draw/Left.tsx
@@ -1,7 +1,25 @@
 const Left = () => {
+    const button = [
+        "logo",
+        "choose",
+        "move",
+        "delete",
+        "point",
+        "line",
+        "constraint2",
+        "constraint1",
+        "load"
+    ];
+    const buttons = button.map((item, idx) => (
+        <li className={item} key={item}>
+            {idx}
+        </li>
+    ));
     return (
-        <div className="left">left</div>
-    )
-}
+        <div className="left">
+            <ul className="leftButton">{buttons}</ul>
+        </div>
+    );
+};
 
-export default Left
+export default Left;
